16422eb5422daffa38a9da15d622c6369c513974
otavio.rodrigues <otavio.rodrigues@c6bank.com>
2019-03-19 11:31:06 -0300

    First running model

--- model.py
+++ model.py
@@ -3,9 +3,6 @@ import torch.nn as nn
 from generate_data import *
 import matplotlib.pyplot as plt
 
-#####################
-# Set parameters
-#####################
 
 # Data params
 noise_var = 0
@@ -13,15 +10,15 @@ num_datapoints = 100
 test_size = 0.2
 num_train = int((1-test_size) * num_datapoints)
 
-# Network params
+
 input_size = 20
-# If `per_element` is True, then LSTM reads in one timestep at a time.
+
 per_element = True
 if per_element:
     lstm_input_size = 1
 else:
     lstm_input_size = input_size
-# size of hidden layers
+
 h1 = 32
 output_dim = 1
 num_layers = 2
@@ -34,7 +31,6 @@ dtype = torch.float
 #####################
 data = ARData(num_datapoints, num_prev=input_size, test_size=test_size, noise_var=noise_var, coeffs=fixed_ar_coefficients[input_size])
 
-# make training and test sets in torch
 X_train = torch.from_numpy(data.X_train).type(torch.Tensor)
 X_test = torch.from_numpy(data.X_test).type(torch.Tensor)
 y_train = torch.from_numpy(data.y_train).type(torch.Tensor).view(-1)
@@ -43,11 +39,7 @@ y_test = torch.from_numpy(data.y_test).type(torch.Tensor).view(-1)
 X_train = X_train.view([input_size, -1, 1])
 X_test = X_test.view([input_size, -1, 1])
 
-#####################
-# Build model
-#####################
 
-# Here we define our model as a class
 class LSTM(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
@@ -58,35 +50,25 @@ class LSTM(nn.Module):
         self.batch_size = batch_size
         self.num_layers = num_layers
 
-        # Define the LSTM layer
         self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
 
-        # Define the output layer
         self.linear = nn.Linear(self.hidden_dim, output_dim)
 
     def init_hidden(self):
-        # This is what we'll initialise our hidden state as
         return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
                 torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
 
     def forward(self, input):
-        # Forward pass through LSTM layer
-        # shape of lstm_out: [input_size, batch_size, hidden_dim]
-        # shape of self.hidden: (a, b), where a and b both
-        # have shape (num_layers, batch_size, hidden_dim).
         lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
-
-        # Only take the output from the final timetep
-        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
         y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
         return y_pred.view(-1)
 
 model = LSTM(lstm_input_size, h1, batch_size=num_train, output_dim=output_dim, num_layers=num_layers)
 
-def loss_new(d, y):
+def loss_new(d, y, a=2, sig=1):
     d = d.unsqueeze(0)
     y = y.unsqueeze(0)
-    gaussian = torch.exp(-1/2. * d.t().repeat(1, d.shape[1]) - y.repeat(y.shape[1], 1)) * 0.5/2.
+    gaussian = torch.exp(-1/2. * d.t().repeat(1, d.shape[1]) - y.repeat(y.shape[1], 1)) * a/(2. * sig)
     loss = torch.sum(gaussian) / d.shape[1]
     return loss
 
@@ -95,19 +77,11 @@ loss_fn = loss_new
 
 optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-
-#####################
-# Train model
-#####################
-
 hist = np.zeros(num_epochs)
 
 for t in range(num_epochs):
-    # Initialise hidden state
-    # Don't do this if you want your LSTM to be stateful
     model.hidden = model.init_hidden()
 
-    # Forward pass
     y_pred = model(X_train)
 
     loss = loss_fn(y_pred, y_train)
@@ -115,18 +89,12 @@ for t in range(num_epochs):
         print("Epoch ", t, "MSE: ", loss.item())
     hist[t] = loss.item()
 
-    # Zero out gradient, else they will accumulate between epochs
     optimiser.zero_grad()
 
-    # Backward pass
     loss.backward()
 
-    # Update parameters
     optimiser.step()
 
-#####################
-# Plot preds and performance
-#####################
 
 plt.plot(y_pred.detach().numpy(), label="Preds")
 plt.plot(y_train.detach().numpy(), label="Data")
