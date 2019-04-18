feac1214663105f3b21cd9eec770518d91fe6179
otavio.rodrigues <otavio.rodrigues@c6bank.com>
2019-04-18 15:34:17 -0300

    First working multivariate loss

--- loss.py
+++ loss.py
@@ -1,5 +1,8 @@
 import torch
 import math
+from test_cov import cov
+
+from scipy.linalg import toeplitz
 
 class CasamentoLoss(torch.nn.Module):
     def __init__(self, sig=1.):
@@ -32,10 +35,42 @@ class MSEControl(torch.nn.Module):
         return torch.sum((d - y) * (d - y))/d.shape[0]
 
 
-cd = CasamentoLoss()
+class CasamentoMult(torch.nn.Module):
+    def __init__(self, sig=1.):
+        super(CasamentoMult, self).__init__()
+        self.sig = sig
+        self.sqrt_pi = math.sqrt(2. * math.pi)
 
-d = torch.Tensor([1.,2.])
-y = torch.Tensor([2.,4.])
+    def forward(self, d, y):
+        d = torch.tensor(toeplitz(d))
+        y = torch.tensor(toeplitz(y))
+
+        d = d.unsqueeze(0)
+        y = y.unsqueeze(0)
+
+        combined = -2 * self.get_component(d, y)
+
+        same_y = self.get_component(y, y)
+
+        same_d = self.get_component(d, d)
+
+        return same_y + same_d + combined
+
+
+    def get_component(self, y1, y2):
+        self.sqrt_pi = math.sqrt(((2. * math.pi) ** y1.shape[1]) * (self.sig ** (2 * y1.shape[1])))
+        d_int = (torch.transpose(y1, 0,1).repeat(1, y2.shape[1], 1) - y2.repeat(y1.shape[1], 1, 1))
+        d_int_transposed = torch.transpose(d_int, 0, 1)
+        print(torch.eye(d_int.shape[1]).inverse().shape)
+        mid = torch.matmul(d_int_transposed, torch.eye(d_int.shape[1]).inverse()) * d_int
+
+        gaussian = torch.exp(-1/2. * mid) * 1/ self.sqrt_pi
+        return torch.sum(gaussian) / (y1.shape[1] * y2.shape[1])
+
+cd = CasamentoMult()
+
+d = torch.Tensor([2.,7.,5.])
+y = torch.Tensor([2.,4.,5.])
 loss = cd(d, y)
 print(loss)
-#loss.backward()
+#loss.backward(7
